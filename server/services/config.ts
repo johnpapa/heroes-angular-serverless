97eb2dd3a7c589e095e5d83116e1e10fa1b0bdf9
John Papa <john@johnpapa.net>
2019-04-30 23:13:06 -0600

    tweak

--- server/services/config.ts
+++ server/services/config.ts
@@ -2,12 +2,12 @@ import { CosmosClient, CosmosClientOptions } from '@azure/cosmos';
 
 const endpoint = process.env.CORE_API_URL || '';
 const masterKey = process.env.CORE_API_KEY || '';
-export const databaseDefName = 'hero-db';
+const databaseDefName = 'hero-db';
 const heroContainerName = 'heroes';
 const villainContainerName = 'villains';
 
 const options: CosmosClientOptions = { endpoint, auth: { masterKey } };
-export const client = new CosmosClient(options);
+const client = new CosmosClient(options);
 
 export const containers = {
   heroes: client.database(databaseDefName).container(heroContainerName),
