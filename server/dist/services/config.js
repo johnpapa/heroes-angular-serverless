32eccc69113ee96fbd698fb5003a05a421b3fde0
John Papa <john@johnpapa.net>
2019-06-23 20:37:27 -0700

    recompile

--- server/dist/services/config.js
+++ server/dist/services/config.js
@@ -3,13 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cosmos_1 = require("@azure/cosmos");
 const endpoint = process.env.CORE_API_URL || '';
 const masterKey = process.env.CORE_API_KEY || '';
-exports.databaseDefName = 'hero-db';
+const databaseDefName = 'hero-db';
 const heroContainerName = 'heroes';
 const villainContainerName = 'villains';
 const options = { endpoint, auth: { masterKey } };
-exports.client = new cosmos_1.CosmosClient(options);
+const client = new cosmos_1.CosmosClient(options);
 exports.containers = {
-    heroes: exports.client.database(exports.databaseDefName).container(heroContainerName),
-    villains: exports.client.database(exports.databaseDefName).container(villainContainerName)
+    heroes: client.database(databaseDefName).container(heroContainerName),
+    villains: client.database(databaseDefName).container(villainContainerName)
 };
 //# sourceMappingURL=config.js.map
